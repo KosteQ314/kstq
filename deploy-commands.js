f0052e11757b378bd3cfbe91f6d32d2933d2b09b
kosteq <76442169+KosteQ314@users.noreply.github.com>
2025-08-10 00:29:21 +0000

    style:separated ready adn interaction create from index.js into their respective files

--- deploy-commands.js
+++ deploy-commands.js
@@ -34,6 +34,7 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
+            // !!! To deploy commands globally simply remove "guildId" from the line below
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
