78854ce56c6565c95ba890573eb089b1dbeb8bf1
kosteq <kostek@wladyka.pl>
2026-05-26 21:12:15 +0200

    docs: add comments describing the command's code

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -4,18 +4,21 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("echo")
         .setDescription("Replies with your input!")
+        // Get the input to echo back
         .addStringOption((option) =>
             option
                 .setName("input")
                 .setDescription("The input to echo back")
                 .setRequired(true),
         )
+        // Select the channel to echo back into
         .addChannelOption((option) =>
             option
                 .setName("channel")
                 .setDescription("The channel to echo back into")
                 .setRequired(true),
         )
+        // Select if the reply should only be visible to the sender
         .addBooleanOption((option) =>
             option
                 .setName("ephemeral")
