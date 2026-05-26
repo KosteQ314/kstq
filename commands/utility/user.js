9e2fdb72e1478452dd88a062ef183361d9b50069
kosteq <kostek@wladyka.pl>
2026-05-26 20:39:55 +0200

    feat: add ability for "user command to show info about anyone

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -3,12 +3,19 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("user")
-        .setDescription("Provides information about the user."),
+        .setDescription("Provides information about the user.")
+        .addUserOption((option) =>
+            option
+                .setName("user")
+                .setDescription("The user to provide information about"),
+        ),
     async execute(interaction) {
+        const user = interaction.options.getUser("user") ?? interaction.user;
+
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
         await interaction.reply(
-            `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`,
+            `This command shows information about ${user}, who joined on ${interaction.member.joinedAt}.`,
         );
     },
 };
